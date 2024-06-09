e76f29bd15c469963d2d2beade175a1d85cd0dd2
mchen <e4572351@gmail.com>
2024-06-09 22:52:28 +0800

    fix errors

--- Assets/Scripts/ViewModel.cs
+++ Assets/Scripts/ViewModel.cs
@@ -45,30 +45,3 @@ public class ViewModel
     }
 
 }
-
-
-public class ModelService
-{
-
-    public Model GetData()
-    {
-        Model m = new Model()
-        {
-            Name = PlayerPrefs.GetString("name"),
-            Job = PlayerPrefs.GetString("job"),
-        };
-
-        return m;
-    }
-
-    public void SaveNmae(string name)
-    {
-        PlayerPrefs.SetString("name", name);
-    }
-
-    public void SaveJob(string job)
-    {
-        PlayerPrefs.SetString("job", job);
-    }
-}
-
